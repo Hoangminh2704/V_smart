013fd5749f28a3693a935c9ef5d81f8bcd8cdcd4
Web-36 <Web-36>
2025-08-12 10:36:05 +0700

    update component

---
+++ Web-6/src/Pages/Order/Order.tsx
@@ -0,0 +1,16 @@
+import Header from "../../Component/Header/Header";
+import Footer from "../../Component/Footer/Footer";
+import Direction from "../../Component/Direction/Direction";
+import Order from "../../Component/Order/Order";
+function OrderPage() {
+  return (
+    <>
+      <Header />
+      <Direction />
+      <Order />
+      <Footer />
+    </>
+  );
+}
+
+export default OrderPage;
